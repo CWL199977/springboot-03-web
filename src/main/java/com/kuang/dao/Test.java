7982ad79aa7407dcca3799d3cbfafd307a3eb89a
chang <cwlcwl.@gmail.com>
2022-07-05 14:12:57 +0800

    第5次提交

--- src/main/java/com/kuang/dao/Test.java
+++ src/main/java/com/kuang/dao/Test.java
@@ -8,5 +8,6 @@ public class Test {
         System.out.println("ddddddddddddd");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
